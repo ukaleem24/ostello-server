d8d6e11bf61ab56f8cc6428009e2ea6bb4cb62ca
ukaleem24 <95086773+ukaleem24@users.noreply.github.com>
2021-12-15 02:53:57 +0500

    Temp changes

--- models/payment.js
+++ models/payment.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const paymentSchema = new Schema({
-  cardholder: { type: Schema.Types.ObjectId, ref: 'User' },
+  booking: { type: Schema.Types.ObjectId, ref: 'Booking' },
+  
+
 });
 
 module.exports = mongoose.model('Booking', paymentSchema);
